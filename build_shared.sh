8f77d57d870f22345e623c7e292fbbca599e1ac3
Björn Bidar <theodorstormgrade@gmail.com>
2019-10-20 00:06:15 +0200

    Fix quoting to prevent word splitting.
    Signed-off-by: Björn Bidar <theodorstormgrade@gmail.com>

--- build_shared.sh
+++ build_shared.sh
@@ -1,12 +1,12 @@
 # Check if mkdtimg tool exists
-if [ ! -f $MKDTIMG ]; then
+if [ ! -f "$MKDTIMG" ]; then
     echo "mkdtimg: File not found!"
     echo "Building mkdtimg"
     export ALLOW_MISSING_DEPENDENCIES=true
     make mkdtimg
 fi
 
-cd $KERNEL_TOP/kernel
+cd "$KERNEL_TOP"/kernel
 
 echo "================================================="
 echo "Your Environment:"
@@ -35,8 +35,8 @@ for platform in $PLATFORMS; do \
     esac
 
     for device in $DEVICE; do \
-        ret=$(rm -rf ${KERNEL_TMP} 2>&1);
-        ret=$(mkdir -p ${KERNEL_TMP} 2>&1);
+        ret=$(rm -rf "${KERNEL_TMP}" 2>&1);
+        ret=$(mkdir -p "${KERNEL_TMP}" 2>&1);
         if [ ! -d ${KERNEL_TMP} ] ; then
             echo "Check your environment";
             echo "ERROR: ${ret}";
@@ -53,7 +53,7 @@ for platform in $PLATFORMS; do \
         echo "The build may take up to 10 minutes. Please be patient ..."
         echo "Building new kernel image ..."
         echo "Logging to $KERNEL_TMP/build_log_${device}"
-        $BUILD >$KERNEL_TMP/build_log_${device} 2>&1;
+        $BUILD >"$KERNEL_TMP"/build_log_${device} 2>&1;
 
         echo "Copying new kernel image ..."
         ret=$(${CP_BLOB}-${device} 2>&1);
@@ -61,7 +61,7 @@ for platform in $PLATFORMS; do \
             *"error"*|*"ERROR"*) echo "ERROR: $ret"; exit 1;;
         esac
         if [ $DTBO = "true" ]; then
-            $MKDTIMG create $KERNEL_TOP/common-kernel/dtbo-$device\.img `find $KERNEL_TMP/arch/arm64/boot/dts -name "*.dtbo"`
+            $MKDTIMG create "$KERNEL_TOP"/common-kernel/dtbo-$device\.img `find $KERNEL_TMP/arch/arm64/boot/dts -name "*.dtbo"`
         fi
     done
 done
