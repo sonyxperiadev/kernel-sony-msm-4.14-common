81ca46e741e275a357d55faa163b401fb170a8e2
Björn Bidar <theodorstormgrade@gmail.com>
2019-12-16 15:29:23 +0100

    Remove $BUILD var and use $BUILD_ARGS to pass additional arguments
    Signed-off-by: Björn Bidar <theodorstormgrade@gmail.com>

--- build_shared.sh
+++ build_shared.sh
@@ -36,23 +36,26 @@ for platform in $PLATFORMS; do \
     esac
 
     for device in $DEVICE; do \
-        rm -rf "${KERNEL_TMP}"
-        mkdir -p "${KERNEL_TMP}"
+        (
+            KERNEL_TMP=$KERNEL_TMP-${device}
+            rm -rf "${KERNEL_TMP}"
+            mkdir -p "${KERNEL_TMP}"
 
-        echo "================================================="
-        echo "Platform -> ${platform} :: Device -> $device"
-        ${BUILD} aosp_$platform"_"$device\_defconfig 2>&1
+            echo "================================================="
+            echo "Platform -> ${platform} :: Device -> $device"
+            make O="$KERNEL_TMP" ARCH=arm64 CROSS_COMPILE="$CROSS_COMPILE" -j$(nproc) ${BUILD_ARGS} aosp_$platform"_"$device\_defconfig
 
-        echo "The build may take up to 10 minutes. Please be patient ..."
-        echo "Building new kernel image ..."
-        echo "Logging to $KERNEL_TMP/build_log_${device}"
-        $BUILD >"$KERNEL_TMP"/build_log_${device} 2>&1;
+            echo "The build may take up to 10 minutes. Please be patient ..."
+            echo "Building new kernel image ..."
+            echo "Logging to $KERNEL_TMP/build.log"
+            make O="$KERNEL_TMP" ARCH=arm64 CROSS_COMPILE="$CROSS_COMPILE" -j$(nproc) ${BUILD_ARGS} >"$KERNEL_TMP/build.log" 2>&1;
 
-        echo "Copying new kernel image ..."
-        ${CP_BLOB}-${device}
-        if [ $DTBO = "true" ]; then
-            $MKDTIMG create "$KERNEL_TOP"/common-kernel/dtbo-$device\.img "$(find "$KERNEL_TMP"/arch/arm64/boot/dts -name "*.dtbo")"
-        fi
+            echo "Copying new kernel image ..."
+            ${CP_BLOB}-${device}
+            if [ $DTBO = "true" ]; then
+                $MKDTIMG create "$KERNEL_TOP"/common-kernel/dtbo-$device\.img "$(find "$KERNEL_TMP"/arch/arm64/boot/dts -name "*.dtbo")"
+            fi
+        )
     done
 done
 
