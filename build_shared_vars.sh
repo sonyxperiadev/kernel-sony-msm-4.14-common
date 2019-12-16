3124acdbae49723a612b339cb89e25ac07526487
Björn Bidar <theodorstormgrade@gmail.com>
2019-12-16 13:22:39 +0100

    Separate build directories per build script
    Like @MarijnS95's idea but simpler.
    
    Signed-off-by: Björn Bidar <theodorstormgrade@gmail.com>

--- build_shared_vars.sh
+++ build_shared_vars.sh
@@ -19,7 +19,8 @@ MKDTIMG=$ANDROID_ROOT/out/host/linux-x86/bin/mkdtimg
 CP_BLOB="cp $KERNEL_TMP/arch/arm64/boot/Image.gz-dtb $KERNEL_TOP/common-kernel/kernel-dtb"
 
 KERNEL_TOP=$ANDROID_ROOT/kernel/sony/msm-4.14
-KERNEL_TMP=$ANDROID_ROOT/out/kernel-tmp
+# $KERNEL_TMP sub dir per script
+KERNEL_TMP=$ANDROID_ROOT/out/${0##*-}/kernel-tmp
 
 export PATH=$CLANG:$PATH:$ANDROID_ROOT/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin
 export PATH=$PATH:$ANDROID_ROOT/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin
