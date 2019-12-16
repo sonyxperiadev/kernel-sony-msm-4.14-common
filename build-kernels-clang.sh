b40112cf2a04c9bd3222d2e3a35b02692a184d78
Björn Bidar <theodorstormgrade@gmail.com>
2019-12-16 15:29:23 +0100

    Remove $BUILD var and use $BUILD_ARGS to pass additional arguments
    Signed-off-by: Björn Bidar <theodorstormgrade@gmail.com>

--- build-kernels-clang.sh
+++ build-kernels-clang.sh
@@ -8,7 +8,7 @@ cd ../../../..
 CLANG_CC="$ANDROID_ROOT/prebuilts/clang/host/linux-x86/clang-r370808/rbin/clang"
 
 # Build command
-BUILD="make O=$KERNEL_TMP ARCH=arm64 CC=$CLANG_CC CLANG_TRIPLE=aarch64-linux-gnu CROSS_COMPILE=$GCC_CC -j$(nproc)"
+BUILD_ARGS="CLANG_TRIPLE=aarch64-linux-gnu CC=$CLANG_CC"
 
 # source shared parts
 . "$OLDPWD/build_shared.sh"
