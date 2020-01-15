8d7b465b5d8ae1f968f666be8dcf48b94402cab4
Björn Bidar <theodorstormgrade@gmail.com>
2020-01-15 05:51:16 +0100

    Use variable substitution/dirname with 0 instead of cd&& $OLDPWD
    It is cleaner and makes the first cd redundant.
    As there's no cd into the $ANDROID_ROOT first we have to find it by
    hand first. Because this project is to be run in an android repo tree
    we can assume that the directory that contains /.repo/ is our $ANDROID_ROOT.
    
    Signed-off-by: Björn Bidar <theodorstormgrade@gmail.com>

--- build-kernels-clang.sh
+++ build-kernels-clang.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-cd ../../../..
-
-. "$OLDPWD/build_shared_vars.sh"
+. "${0%/*}/build_shared_vars.sh"
 
 # Cross Compiler
 CLANG_CC="$ANDROID_ROOT/prebuilts/clang/host/linux-x86/clang-r370808/rbin/clang"
@@ -11,4 +9,4 @@ CLANG_CC="$ANDROID_ROOT/prebuilts/clang/host/linux-x86/clang-r370808/rbin/clang"
 BUILD_ARGS="CLANG_TRIPLE=aarch64-linux-gnu CC=$CLANG_CC"
 
 # source shared parts
-. "$OLDPWD/build_shared.sh"
+. "${0%/*}/build_shared.sh"
