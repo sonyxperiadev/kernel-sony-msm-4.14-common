9fe7324183216535b57c373ba06f1cce326066a4
Björn Bidar <theodorstormgrade@gmail.com>
2020-01-15 05:51:16 +0100

    Use variable substitution/dirname with 0 instead of cd&& $OLDPWD
    It is cleaner and makes the first cd redundant.
    As there's no cd into the $ANDROID_ROOT first we have to find it by
    hand first. Because this project is to be run in an android repo tree
    we can assume that the directory that contains /.repo/ is our $ANDROID_ROOT.
    
    Signed-off-by: Björn Bidar <theodorstormgrade@gmail.com>

--- build_shared_vars.sh
+++ build_shared_vars.sh
@@ -1,3 +1,17 @@
+find_repo_root()
+# desc: Find root of a repo tree and echo it
+{
+    (
+        max_depth=10
+        while ! [ -e "$PWD/.repo" ] ; do
+            [ $max_depth -eq 0 ] && return 1
+            cd ..
+            max_depth=$((max_depth-1))
+        done
+        echo "$PWD"
+    )
+}
+
 usage(){
     cat <<EOF
 Build kernel for supported devices
@@ -21,7 +35,8 @@ while getopts $arguments argument ; do
 done
 
 if [ -z "$ANDROID_BUILD_TOP" ]; then
-    ANDROID_ROOT=$(realpath "$PWD")
+    ANDROID_ROOT=$(find_repo_root)
+    ANDROID_ROOT=$(realpath "$ANDROID_ROOT")
     echo "ANDROID_BUILD_TOP not set, guessing root at $ANDROID_ROOT"
 else
     ANDROID_ROOT="$ANDROID_BUILD_TOP"
