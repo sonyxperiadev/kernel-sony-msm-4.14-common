602f1b683f49ab2b10ed633e05cf15151f8dec1e
Alin Jerpelea <alin.jerpelea@sony.com>
2020-02-20 11:51:37 +0100

    build-kernels: include compilers in PATH
    Add Compilers to PATH since there is no need to export compilers explicaitly
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- build-kernels-clang.sh
+++ build-kernels-clang.sh
@@ -1,17 +1,18 @@
 cd ../../../..
+
 export ANDROID_ROOT=$(pwd)
 export KERNEL_TOP=$ANDROID_ROOT/kernel/sony/msm-4.14
 export KERNEL_TMP=$ANDROID_ROOT/out/kernel-tmp
+export CC=$ANDROID_ROOT/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/
+export CLANG=$ANDROID_ROOT/prebuilts/clang/host/linux-x86/clang-r353983c/bin/
 
-# Cross Compiler
-export GCC_CC=$ANDROID_ROOT/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
-export CLANG_CC=$ANDROID_ROOT/prebuilts/clang/host/linux-x86/clang-r353983c/bin/clang
+export PATH=$CC:$CLANG:$PATH
 
 # Mkdtimg tool
 export MKDTIMG=$ANDROID_ROOT/out/host/linux-x86/bin/mkdtimg
 
 # Build command
-export BUILD="make O=$KERNEL_TMP ARCH=arm64 CC=$CLANG_CC CLANG_TRIPLE=aarch64-linux-gnu CROSS_COMPILE=$GCC_CC -j$(nproc)"
+export BUILD="make O=$KERNEL_TMP ARCH=arm64 CC=clang CLANG_TRIPLE=aarch64-linux-gnu- CROSS_COMPILE=aarch64-linux-android- -j$(nproc)"
 
 # Copy prebuilt kernel
 export CP_BLOB="cp $KERNEL_TMP/arch/arm64/boot/Image.gz-dtb $KERNEL_TOP/common-kernel/kernel-dtb"
