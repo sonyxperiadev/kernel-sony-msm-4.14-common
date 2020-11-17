771af8519780e9576e4f6ad41a355fc268251525
Björn Bidar <bjorn.bidar@jolla.com>
2020-11-17 19:00:23 +0200

    build-kernel: refactor variables to the top
    This reduces the need of using intermediate variables to override them
    if needed.
    
    Signed-off-by: Björn Bidar <bjorn.bidar@jolla.com>

--- build_shared_vars.sh
+++ build_shared_vars.sh
@@ -1,3 +1,12 @@
+YOSHINO="lilac maple poplar"
+NILE="discovery pioneer voyager"
+GANGES="kirin mermaid"
+TAMA="akari apollo akatsuki"
+KUMANO="griffin bahamut"
+SEINE="pdx201"
+
+PLATFORMS="yoshino nile ganges tama kumano seine"
+
 find_repo_root()
 # desc: Find root of a repo tree and echo it
 {
@@ -24,18 +33,6 @@ Options:
 EOF
 }
 
-
-arguments=khd:O:
-while getopts $arguments argument ; do
-    case $argument in
-        k) keep_kernel_tmp=t ;;
-        d) only_build_for=$OPTARG;;
-        O) build_directory=$OPTARG;;
-        h) usage; exit 0;;
-        ?) usage; exit 1;;
-    esac
-done
-
 if [ -z "$ANDROID_BUILD_TOP" ]; then
     ANDROID_ROOT=$(find_repo_root)
     ANDROID_ROOT=$(realpath "$ANDROID_ROOT")
@@ -44,21 +41,25 @@ else
     ANDROID_ROOT="$ANDROID_BUILD_TOP"
 fi
 
-YOSHINO="lilac maple poplar"
-NILE="discovery pioneer voyager"
-GANGES="kirin mermaid"
-TAMA="akari apollo akatsuki"
-KUMANO="griffin bahamut"
-SEINE="pdx201"
-
-PLATFORMS="yoshino nile ganges tama kumano seine"
+PATH=$PATH:$ANDROID_ROOT/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin
+PATH=$PATH:$ANDROID_ROOT/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin
 
 # Mkdtimg tool
 MKDTIMG=$ANDROID_ROOT/out/host/linux-x86/bin/mkdtimg
 
 KERNEL_TOP=$ANDROID_ROOT/kernel/sony/msm-4.14
 # $KERNEL_TMP sub dir per script
-KERNEL_TMP=${build_directory:-$ANDROID_ROOT/out/${0##*-}/kernel-tmp}
+KERNEL_TMP=$ANDROID_ROOT/out/${0##*-}/kernel-tmp
 
-export PATH=$PATH:$ANDROID_ROOT/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin
-export PATH=$PATH:$ANDROID_ROOT/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin
+arguments=khd:O:
+while getopts $arguments argument ; do
+    case $argument in
+        k) keep_kernel_tmp=t ;;
+        d) only_build_for=$OPTARG;;
+        O) KERNEL_TMP=$OPTARG;build_directory=$OPTARG;;
+        h) usage; exit 0;;
+        ?) usage; exit 1;;
+    esac
+done
+
+export PATH
